b8791c10252a95d975675cf955b7732cceaafcf8
shridharmali88@gmail.com <shridharmali88@gmail.com>
2025-12-25 20:02:39 +0000

    added udf

--- utils/function-utils.py
+++ utils/function-utils.py
@@ -1,2 +1,17 @@
 # Databricks notebook source
+# DBTITLE 1,UDF in DF
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 
+@F.udf(T.StringType())
+def lower_name(name):
+    return name.lower()
+
+df = spark.createDataFrame([('Jen', 2), ('Bob', 4), ('Zoe', 5)], ['name', 'age'])
+display(df.select(lower_name(df.name).alias('lower_name'), '*'))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- EXPLAIN FORMATTED 
+# MAGIC
